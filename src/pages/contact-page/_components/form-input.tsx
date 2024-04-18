a9a97bc418be90f9f614efcf098a00817493916b
Andrian004 <128672158+Andrian004@users.noreply.github.com>
2024-04-18 20:03:59 +0700

    feat: create form-input & form-textarea

---
+++ src/pages/contact-page/_components/form-input.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent } from "react";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+
+type FormInputProps = {
+  label: string;
+  id: string;
+  type?: string;
+  placeholder?: string;
+  inputStyle?: string;
+  labelStyle?: string;
+  rootStyle?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export function FormInput({
+  label,
+  id,
+  type = "text",
+  placeholder,
+  inputStyle,
+  labelStyle,
+  rootStyle,
+  onChange,
+}: FormInputProps) {
+  return (
+    <div className={cn("space-y-2", rootStyle)}>
+      <label
+        htmlFor={id}
+        className={cn(
+          "text-lg lg:text-xl after:content-['*'] after:text-red-600",
+          labelStyle
+        )}
+      >
+        {label}
+      </label>
+      <Input
+        id={id}
+        type={type}
+        className={cn("text-lg bg-neutral-200", inputStyle)}
+        placeholder={placeholder}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
---
+++ src/pages/contact-page/_components/form-textarea.tsx
@@ -0,0 +1,43 @@
+import { ChangeEventHandler } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
+
+type FormTextareaProps = {
+  id: string;
+  label: string;
+  placeholder?: string;
+  rootStyle?: string;
+  labelStyle?: string;
+  textareaStyle?: string;
+  onChange?: ChangeEventHandler<HTMLTextAreaElement> | undefined;
+};
+
+export function FormTextarea({
+  id,
+  label,
+  placeholder,
+  rootStyle,
+  labelStyle,
+  textareaStyle,
+  onChange,
+}: FormTextareaProps) {
+  return (
+    <div className={cn("space-y-2", rootStyle)}>
+      <label
+        htmlFor={id}
+        className={cn(
+          "text-lg lg:text-xl after:content-['*'] after:text-red-600",
+          labelStyle
+        )}
+      >
+        {label}
+      </label>
+      <Textarea
+        id={id}
+        placeholder={placeholder}
+        className={cn("text-lg bg-neutral-200", textareaStyle)}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
